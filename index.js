7fb6b4aacf2bfbe6bc846859c0f24a11874ffcea
timebase123 <137128244+timebase123@users.noreply.github.com>
2023-06-20 10:09:08 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var age = 18;
+var gender = "male";
+if (age >= 18 && gender == "male") {
+    console.log("you are a ")
+}else if (age >= 18 && gender == "female") {
+    console.log("you are a men");
+}else {
+    console.log("you are a kid ")
+}
